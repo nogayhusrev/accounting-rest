ba379370b454a3ce33ab71e6ab2d3f7fa1cc9bbc
nogayhusrev <nogayhusrev@gmail.com>
2023-07-08 21:53:50 +0300

    Initial Commit

---
+++ src/main/java/com/nogayhusrev/accountingrest/AccountingRestApplication.java
@@ -0,0 +1,13 @@
+package com.nogayhusrev.accountingrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountingRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountingRestApplication.class, args);
+    }
+
+}
